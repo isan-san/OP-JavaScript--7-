a3ca5078f181e2b0ff90165ce581a14e787f8a4b
isan-san <isan.9.9f@gmail.com>
2022-12-14 14:25:17 -0500

    Finish exercice 3

--- Ejercicio3.js
+++ Ejercicio3.js
@@ -1,1 +1,18 @@
 
+const hoy = new Date();
+
+console.log(hoy);
+
+const birthdate = new Date(1999,3,20);
+
+console.log(birthdate);
+
+const isGreater = hoy.getTime() > birthdate.getTime();
+
+console.log(isGreater);
+
+const dayOfBirth = birthdate.getDate();
+const monthOfBirth = birthdate.getMonth();
+const yearOfBirth = birthdate.getFullYear();
+
+console.log("My birthdate is: "+dayOfBirth+" / " + monthOfBirth+" / " + yearOfBirth);
