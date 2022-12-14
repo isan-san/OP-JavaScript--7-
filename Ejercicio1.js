76f54213391f7d0ce8b762fa7eedf697a5e8f752
isan-san <isan.9.9f@gmail.com>
2022-12-14 13:59:13 -0500

    Initial commit

---
+++ Ejercicio1.js
@@ -0,0 +1,11 @@
+const mySet =  new Set (["isan", "maria", "lugel", "catalina"]);
+
+console.log(mySet);
+
+mySet.add ("isan");
+
+console.log(mySet);
+
+mySet.add ("JavaScript");
+
+console.log(mySet);
